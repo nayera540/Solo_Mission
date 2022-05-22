ab9c68aa6e548857d8f60ebb16a248f404ba901c
nayera540 <69148381+nayera540@users.noreply.github.com>
2022-05-22 13:50:19 +0200

    Update Traffic_light_controller.ino

--- Traffic_light_controller/Traffic_light_controller.ino
+++ Traffic_light_controller/Traffic_light_controller.ino
@@ -1,13 +1,36 @@
+#define red 13
+#define yellow 12
+#define green 11
+#define push_button 10
+
+int state;
+
 void setup() {
-  pinMode(13,OUTPUT);
+  pinMode(red,OUTPUT);
+  pinMode(yellow,OUTPUT);
+  pinMode(green,OUTPUT);
+  pinMode(push_button,INPUT);
 
 }
 
 void loop() {
-  digitalWrite(13,HIGH);
-  delay(500);
-  digitalWrite(13,LOW);
-  delay(500);
+  state = digitalRead(push_button);
+  
+  if(state == 1){//push_button is not pressed
+      digitalWrite(red,HIGH);
+      digitalWrite(yellow,HIGH);
+      digitalWrite(green,HIGH);
+      
+   
+    }
+
+    else{
+      digitalWrite(red,LOW);
+      digitalWrite(yellow,LOW);
+      digitalWrite(green,LOW);
+      
+      
+      }
   
 
 }
